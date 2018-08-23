d16e84c053c80d974e052d1c0639ea7dc4c33f57
draghici-alexandru <alex.draghici@cst-dev.com>
2018-08-23 11:34:11 +0300

    App-rounting for the login and register component; adding the page not found component; redirecting to the page not found if the route doesn't exist

--- proiect/src/app/app-routing.module.ts
+++ proiect/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent, LoginComponent } from './authentication/index';
+import { PageNotFoundComponent } from './shared/index';
 
 const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: '**',  component: PageNotFoundComponent }
 ];
 
 @NgModule({
