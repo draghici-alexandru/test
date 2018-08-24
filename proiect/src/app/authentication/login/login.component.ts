53e7770e79930ec97e0e7b5b03dbf54c651a1f69
draghici-alexandru <alex.draghici@cst-dev.com>
2018-08-24 11:02:21 +0300

    Added the login form

--- proiect/src/app/authentication/login/login.component.ts
+++ proiect/src/app/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
 
+  constructor(fb: FormBuilder) {
+
+    this.loginForm = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      remember: ['']
+    });
+  }
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
   }
 
 }
